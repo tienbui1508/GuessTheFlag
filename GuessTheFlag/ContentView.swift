2a514c8eee08614678fc1d96d18fa3de3bf78b15
tienbui <tienbui1508@gmail.com>
2023-05-18 23:19:00 +0930

    - add reset function to reset the game after 8 guesses - fix repeated questions in 1 game

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var showingResult = false
+    @State private var scoreTitle = ""
+    @State private var countries = allCountries.shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var questionCounter = 1
+
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.7, blue: 0.5), location: 0.3),
+                .init(color: Color(red: 0.7, green: 0.6, blue: 0.4), location: 0.3)
+            ], center: .top, startRadius: 100, endRadius: 700)
+                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                Text("Guess the flag")
+                    .font(.title)
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.headline)
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAnswer])
+                            .font(.title)
+                    }
+
+                    VStack(spacing: 20) {
+                        ForEach(0 ..< 3) { index in
+                            Button {
+                                flagTapped(index)
+                            } label: {
+                                Image(countries[index])
+                                    .shadow(radius: 5)
+                            }
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+
+                Spacer()
+
+                Text("Score: \(score)/\(questionCounter)")
+                    .font(.title)
+                    .bold()
+                Spacer()
+            }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                Text("Your score is \(score)")
+            }
+            .alert("Game over!", isPresented: $showingResult) {
+                Button("Play again", action: resetGame)
+            } message: {
+                Text("Your final score is \(score)")
+            }
+            .padding()
         }
-        .padding()
+    }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            let needsThe = ["UK", "US"]
+            if needsThe.contains(countries[number]) {
+                scoreTitle = "Wrong! That's the flag of the \(countries[number])"
+            } else {
+                scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            }
+        }
+        showingScore = true
+        if questionCounter == 8 {
+            showingResult = true
+        }
+    }
+
+    func askQuestion() {
+        if !showingResult {
+            countries.remove(at: correctAnswer)
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+            questionCounter += 1
+        }
+    }
+
+    func resetGame() {
+        score = 0
+        questionCounter = 0
+        countries = Self.allCountries
+        askQuestion()
     }
 }
 
